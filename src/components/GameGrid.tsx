22af9139bc655fe98dff7aa01ed50a8f61d460be
Neil Mills <mail@neil-mills.co.uk>
2023-05-24 11:47:41 +0100

    Create custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,31 +2,10 @@ import React, { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { AxiosError } from 'axios';
 import { Text } from '@chakra-ui/react';
+import useGames from '../hooks/useGames';
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-  useEffect(() => {
-    const getGames = async () => {
-      try {
-        const res = await apiClient.get<FetchGamesResponse>('/xgames');
-        setGames(res.data.results);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
-    getGames();
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { AxiosError, CanceledError } from 'axios';
+import { useState, useEffect } from 'react';
+import apiClient from '../services/api-client';
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const getGames = async () => {
+      try {
+        setIsLoading(true);
+        const res = await apiClient.get<FetchGamesResponse>('/games', {
+          signal: controller.signal,
+        });
+        setGames(res.data.results);
+        setIsLoading(false);
+      } catch (err) {
+        if (err instanceof CanceledError) return;
+        setError((err as AxiosError).message);
+        setIsLoading(false);
+      }
+    };
+    getGames();
+    return () => controller.abort();
+  });
+  return { games, error, isLoading };
+};
+
+export default useGames;
