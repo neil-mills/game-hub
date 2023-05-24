a33b61e402faba7bf218b1302bef625638788665
Neil Mills <mail@neil-mills.co.uk>
2023-05-24 09:51:45 +0100

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HStack, Image, Text } from '@chakra-ui/react';
+import Logo from '../assets/logo.webp';
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={Logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
